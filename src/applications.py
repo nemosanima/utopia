25402071450457cccaa9f70445d79816f32e3b34
nemosanima <nemosanima@gmail.com>
2024-01-22 13:57:23 +0300

    feat: ability to create handlers

--- src/applications.py
+++ src/applications.py
@@ -1,8 +1,45 @@
+import json
+
+
 class App:
+    def __init__(self):
+        self.routers = {}
+
     def __call__(self, environ, start_response):
-        data = b"Hello, World!\n"
-        start_response("200 OK", [
-            ("Content-Type", "application/json; charset=utf-8"),
-            ("Content-Length", str(len(data)))
-        ])
-        return iter([data])
+        handler = self.find_handler(environ['PATH_INFO'])
+        if handler:
+            data = handler()
+            response_body = json.dumps(data).encode("utf-8")
+            status_code = "200"
+        else:
+            response_body, status_code = self.not_found_response()
+
+        start_response(
+            status_code,
+            [
+                ("Content-Type", "application/json; charset=utf-8"),
+                ("Content-Length", str(len(response_body)))
+            ]
+        )
+        return iter([response_body])
+
+    def router(self, path: str):
+        def wrapper(handler):
+            self.routers[path] = handler
+            return handler
+        return wrapper
+
+    def find_handler(self, requested_path: str):
+        for path, handler in self.routers.items():
+            if path == requested_path:
+                return handler
+        return None
+
+    def not_found_response(self):
+        return b"Not Found", "404"
+
+
+
+
+
+
