44becfaf12addd53de9e238f4b1d38534757e957
nemosanima <nemosanima@gmail.com>
2024-01-22 13:11:53 +0300

    feat: class entry point

--- src/applications.py
+++ src/applications.py
@@ -1,7 +1,8 @@
-def app(environ, start_response):
-    data = b"Hello, World!\n"
-    start_response("200 OK", [
-        ("Content-Type", "text/plain"),
-        ("Content-Length", str(len(data)))
-    ])
-    return iter([data])
+class App:
+    def __call__(self, environ, start_response):
+        data = b"Hello, World!\n"
+        start_response("200 OK", [
+            ("Content-Type", "application/json; charset=utf-8"),
+            ("Content-Length", str(len(data)))
+        ])
+        return iter([data])
